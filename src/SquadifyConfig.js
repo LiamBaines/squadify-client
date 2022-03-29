6eb64092e5ec614b1f6e8e5a74cb21417311c5a6
LiamBaines <liam@thebaines.com>
2022-03-29 23:22:16 +0100

    "Remove squadify-ui folder"

--- src/SquadifyConfig.js
+++ src/SquadifyConfig.js
@@ -1,6 +1,7 @@
 const config = {
 
-  baseUrl: "http://192.168.0.183",
+  baseUrl: "http://192.168.0.189",
+  clientId: "434ddce9aa094b19abf5aeced0a5b1d7",
 
   baseUrlEscaped() {
     return this.baseUrl.replace("://", "%3A%2F%2F")
--- src/auth/redirectToSpotifyLogin.js
+++ src/auth/redirectToSpotifyLogin.js
@@ -3,7 +3,7 @@ import config from "../SquadifyConfig";
 function redirectToSpotifyLogin() {
   console.log("handling 401 @ " + window.location.pathname);
   window.location.href = "https://accounts.spotify.com/authorize"
-  + "?client_id=" + config.clientId()
+  + "?client_id=" + config.clientId
   + "&response_type=code"
   + "&scope=playlist-modify-public%20playlist-modify-private%20ugc-image-upload%20playlist-read-private%20playlist-read-collaborative%20user-library-read%20user-read-private%20user-top-read"
   + "&state=" + window.location.pathname 
